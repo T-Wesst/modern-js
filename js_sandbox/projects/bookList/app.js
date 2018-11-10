1af65777966742e84fc499f40eef51d43f498056
T-Wesst <code.daddy17@gmail.com>
2018-11-10 01:34:31 -0800

    add book to list logic

---
+++ js_sandbox/projects/bookList/app.js
@@ -0,0 +1,48 @@
+// create book constructor | handles creating book object
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+// create UI constructor | set of prototype methods [addBookToList, deleteBook, showAlert]
+function UI() {}
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById("book-list");
+  // create tr element
+  const row = document.createElement("tr");
+  // insert columns
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class = "delete"> X </a></td>
+  `;
+  list.appendChild(row);
+};
+
+// clear fields
+UI.prototype.clearFields = function() {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
+// create Event Listeners
+document.getElementById("book-form").addEventListener("submit", function(e) {
+  // get form values [title, author, isbn]
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+  // instantiate book
+  const book = new Book(title, author, isbn);
+  // instantiate UI
+  const ui = new UI();
+  // add book to list
+  ui.addBookToList(book);
+  // clear book fields
+  ui.clearFields();
+
+  console.log(book);
+
+  e.preventDefault();
+});

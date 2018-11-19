2a2f2c582e73b566a18ca35be00f4f3094ad282d
T-Wesst <code.daddy17@gmail.com>
2018-11-19 01:32:02 -0800

    add promise notes

---
+++ js_sandbox/notes/asyncJS-ajax-api/promises.js
@@ -0,0 +1,38 @@
+// hard coded "server"
+const posts = [
+  { title: "post one", body: "this is post one" },
+  { title: "post two", body: "this is post two" }
+];
+
+function createPost(post) {
+  // resolve = is done && reject = throw err
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      posts.push(post);
+      const error = true;
+      if (!error) {
+        resolve();
+      } else {
+        reject("Error: Something went wrong");
+      }
+    }, 2000);
+  });
+}
+function getPosts() {
+  setTimeout(function() {
+    let output = "";
+    posts.forEach(function(post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+// .then()
+createPost({
+  title: "post three",
+  body: "this is post three"
+})
+  .then(getPosts)
+  .catch(function(err) {
+    console.log(err); // logs "Error: Something went wrong"
+  });

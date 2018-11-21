6c42000eca2a2357fff7a7cd55bdc50c8439e164
T-Wesst <code.daddy17@gmail.com>
2018-11-21 11:42:05 -0800

    add comments

--- js_sandbox/projects/githubFinder/app.js
+++ js_sandbox/projects/githubFinder/app.js
@@ -9,7 +9,18 @@ document.getElementById("searchUser").addEventListener("keydown", getUser);
 
 function getUser() {
   // create XHR object
-  const xhr = new XHRHttpRequest();
-  // xhr.open get request
+  const xhr = new XMLHttpRequest();
+  // get request
   xhr.open("GET", "https://api.github.com/users", true);
+  xhr.onload = function() {
+    if (this.status === 200) {
+      // parse JSON
+      const response = JSON.parse(this.responseText);
+      debugger;
+      console.log(response); // returns Array of objects
+      console.log(response[0]); // returns JS object at index 0
+      console.log(response[0].login); // returns username
+    }
+  };
+  xhr.send();
 }

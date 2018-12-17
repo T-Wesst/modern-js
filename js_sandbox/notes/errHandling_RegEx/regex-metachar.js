8fe8e372b451f56f8883a13b2e85ff5c01b35e6d
T-Wesst <code.daddy17@gmail.com>
2018-12-16 18:02:15 -0800

    add character sets, quantifiers, and grouping notes

--- js_sandbox/notes/errHandling_RegEx/regex-metachar.js
+++ js_sandbox/notes/errHandling_RegEx/regex-metachar.js
@@ -12,6 +12,23 @@ regex = /h*llo/i; // matches any character 0 or more times
 regex = /gra?e?y/i; // optional character
 regex = /gra?e?y\?/i; // escape character
 
+// Brackets [] - chracter sets
+regex = /gr[ae]y/i; // must be an a or e
+regex = /[GF]ray/; // must be G or F
+regex = /[^GF]ray/i; // match anything except G or F
+regex = /[A-Z]ray/; // match any uppercase letter
+regex = /[a-z]ray/; // match any lowercase letter
+regex = /[A-Za-z]ray/; // match any letter
+regex = /[0-9]ray/; // match any digit
+
+// Braces {} - quantifiers
+regex = /hel{2}o/i; // must occur exactly {x} amount of times
+regex = /hel{2,4}o/i; // must occur between {x} amount of times
+regex = /hel{2,}o/i; // must occur >= {x} times
+
+// parenthesis () - grouping
+regex = /^([0-9]x){3}$/i; // must be exactly {numX} 3 times
+
 // string to match
 const str = "Hello World";
 // log resulta

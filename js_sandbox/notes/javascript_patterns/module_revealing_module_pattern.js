0f2c6f64b92803ca7c16b7275560197584d65915
T-Wesst <code.daddy17@gmail.com>
2019-01-15 23:10:45 -0800

    js pattern notes

---
+++ js_sandbox/notes/javascript_patterns/module_revealing_module_pattern.js
@@ -0,0 +1,51 @@
+// Structure
+
+// IIFE immediately invoked function
+(function() {
+  // declare private variables and functions
+  return {
+    // declare public variables and functions
+  };
+})();
+
+// STANDARD MODULE PATTERN
+const UiCtrl = (function() {
+  // private
+  let text = 'Hello World';
+  const changeText = function() {
+    const element = document.querySelector('h1');
+    element.textContent = text;
+  };
+// public
+  return {
+    callChangeText = function(){
+      changeText();
+      console.logg(text);
+    }
+  }
+})();
+
+UiCtrl.callChangeText(); // logs hello world and changes h1 to hello world
+
+//  REVEALING MODULE PATTERN
+const ItemCtrl = (function(){
+  let data = [];
+  function add(item){
+    data.push(item);
+    console.log('item added');
+  }
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    });
+  }
+
+  return {
+    add: add,
+    get: get
+  }
+})()
+
+ItemCtrl.add({id: 1, name: 'John'}) // logs Item added
+console.log(ItemCtrl.get(1))

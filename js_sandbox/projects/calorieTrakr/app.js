27fe8e1074c4f59a11138a67ff45ab7f0e77441c
T-Wesst <code.daddy17@gmail.com>
2019-02-23 23:22:05 -0800

    delete from local storage

--- js_sandbox/projects/calorieTrakr/app.js
+++ js_sandbox/projects/calorieTrakr/app.js
@@ -37,6 +37,15 @@ const StorageCtrl = (function() {
         }
       });
       localStorage.setItem('items', JSON.stringify(items));
+    },
+    deleteItemFromStorage: function(id) {
+      let items = JSON.parse(localStorage.getItem('items'));
+      items.forEach(function(item, index) {
+        if (id === item.id) {
+          items.splice(index, 1);
+        }
+      });
+      localStorage.setItem('items', JSON.stringify(items));
     }
   };
 })();
@@ -373,6 +382,8 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
     const totalCalories = ItemCtrl.getTotalCalories();
     // add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
+    // delete from local storage
+    StorageCtrl.deleteItemFromStorage(currentItem.id);
     UICtrl.clearEditState();
     e.preventDefault();
   };

58b25cbe278f59e678744a761e1cc8260e7fadeb
T-Wesst <code.daddy17@gmail.com>
2019-04-16 16:11:36 -0700

    copy easyHTTP solution

---
+++ js_sandbox/projects/microPosts/src/http.js
@@ -0,0 +1,45 @@
+class HTTP {
+  // GET request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+  // POST request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+  // PUT request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+  // DELETE request
+  async delete(url) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+    const resData = await 'Resource deleted..';
+    return resData;
+  }
+}
+
+export const http = new HTTP();

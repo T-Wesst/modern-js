04f242a08a9fd0f2c956c2c8357dc958a32674b0
T-Wesst <code.daddy17@gmail.com>
2019-04-30 11:33:58 -0700

    add & create post

--- js_sandbox/projects/microPosts/src/app.js
+++ js_sandbox/projects/microPosts/src/app.js
@@ -3,10 +3,29 @@ import { ui } from './ui.js';
 
 // get post on dom load
 document.addEventListener('DOMContentLoaded', getPosts);
+// listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// get posts
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+// add post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+  const data = { title, body };
+
+  // create post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post Added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
     .catch(err => console.log(err));
 }
